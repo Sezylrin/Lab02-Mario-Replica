937429b49ec43a72d781b7bb734614f8f7c9c56c
Sezylrin <89729539+Alastair-Lu@users.noreply.github.com>
2023-03-04 22:37:34 +1100

    Fire Flower Nuance
    Fire Flower behavior when spawned

--- Assets/Scripts/FireBall.cs
+++ Assets/Scripts/FireBall.cs
@@ -9,18 +9,27 @@ public class FireBall : MonoBehaviour
     
     private Vector2 Direction = Vector2.right;
     private Rigidbody2D Rigid2D;
+    private bool bounce = false;
+    private float Gravity;
 
 
     // Start is called before the first frame update
     void Start()
     {
         Rigid2D = GetComponent<Rigidbody2D>();
+        Gravity = Rigid2D.gravityScale;
+        Rigid2D.gravityScale = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Direction * Speed * Time.deltaTime);
+        if (bounce == true)
+            transform.Translate(Direction * Speed * Time.deltaTime);
+        else
+        {
+            Rigid2D.velocity = (Direction + Vector2.down) * Speed;
+        }
     }
 
     public void SetDirection(Vector2 Dir)
@@ -39,6 +48,11 @@ public class FireBall : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
+        if(bounce == false)
+        {
+            bounce = true;
+            Rigid2D.gravityScale = Gravity;
+        }
         if (Rigid2D.velocity.y <= 0)
         {
             Rigid2D.AddForce(Vector2.up * Impulse, ForceMode2D.Impulse);
