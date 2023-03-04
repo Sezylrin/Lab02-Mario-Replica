5ad3539035d3c2b9f1b6f2696bb9d7b0bd4577fc
Sezylrin <89729539+Alastair-Lu@users.noreply.github.com>
2023-03-04 18:49:20 +1100

    Fire Flower Function Implemented
    Functional Fire Flower projectile

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,14 +6,19 @@ public class PlayerManager : MonoBehaviour
 {
     public int PlayerState = 1;
     public float InvincibleTimer = 8;
+    public float ShotDelay = 0.25f;
     public Sprite[] Sprites;
     public GameObject MarioSprite;
+    public GameObject FireProjectile;
+    public float offset;
 
+    private Vector2 Direction = Vector2.left;
     private Animator MarioAnimator;
     private BoxCollider2D Collider;
     private SpriteRenderer SpriteRender;
     private bool Invincible = false;
     private float Timer;
+    private float FireTimer = 0;
     // Start is called before the first frame update
     void Start()
     {        
@@ -25,12 +30,23 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (FireTimer > 0)
+            FireTimer--;
         if (Invincible == true)
         {
             Timer -= Time.deltaTime;
             if (Timer < 0)
             {
                 Invincible = false;
+            }
+        }
+        if (PlayerState == 3)
+        {
+            if (Input.GetKeyDown(KeyCode.X) && FireTimer <= 0)
+            {
+                GameObject Fireball = Instantiate(FireProjectile, transform.position + (new Vector3 (offset * Direction.x, 0.5f,0)),Quaternion.identity);
+                Fireball.GetComponent<FireBall>().SetDirection(Direction);
+                FireTimer = ShotDelay;
             }
         }
     }
@@ -58,7 +74,7 @@ public class PlayerManager : MonoBehaviour
                 //game manager icriment lives
             }
             MarioAnimator.Play("Grow", 0, 0);
-            MarioSprite.transform.Translate(Vector3.up * (PlayerState > 1 ? 0.5f : 0), Space.Self);
+            MarioSprite.transform.Translate(Vector3.up * (PlayerState == 2 ? 0.5f : 0), Space.Self);
             Collider.size = new Vector2(1, (PlayerState > 1 ? 2 : 1));
             Collider.offset = new Vector2(0, (PlayerState > 1 ? 0.5f : 0));
             Destroy(collision.gameObject);
