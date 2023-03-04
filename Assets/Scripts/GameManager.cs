a9cc4d0fb7624bbc61c72cc04256600e0a9d9d0c
EdoardoMenon <edoardomenon1997@gmail.com>
2023-03-04 21:13:57 +1100

    Add GameManager with UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("TextFields")]  
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text coinText;
+    [SerializeField] private TMP_Text timerText;
+    [SerializeField] private TMP_Text worldText;
+
+    [Header("Timer")]
+    [SerializeField] private float startingTime;
+    [SerializeField] private int timerSpeed = 3;
+
+    [Header("World")]
+    [SerializeField] private string world;
+
+    private int coinsCollected = 0;
+    private int score = 0;
+    private float currentTime;
+
+    private void Start()
+    {
+        currentTime = startingTime;
+        worldText.text = world;
+    }
+
+    private void Update()
+    {
+        RunTimer();
+    }
+
+    private void RunTimer()
+    {
+        currentTime -= timerSpeed * Time.deltaTime;
+        timerText.text = Mathf.RoundToInt(currentTime).ToString(); ;
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            //TODO: Display gameover screen
+        }
+    }
+
+    public void AddToScore(int amountToIncrease)
+    {
+        int newScore = score + amountToIncrease;
+        score = newScore;
+        scoreText.text = newScore.ToString("D6");
+    }
+
+    public int GetScore() { return score; }
+
+
+    public void IncrementCoinCount(int amountToIncrease = 1)
+    {
+        int newCoinsCollected = coinsCollected + amountToIncrease;
+        coinsCollected = newCoinsCollected;
+        coinText.text = coinsCollected.ToString("D2");
+    }
+
+    public int GetCoinsCollected() { return coinsCollected; }
+}
