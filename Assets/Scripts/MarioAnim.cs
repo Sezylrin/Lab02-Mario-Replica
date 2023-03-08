e96f99681b3a7d7acb112dd98c734057416621f2
Sezylrin <89729539+Alastair-Lu@users.noreply.github.com>
2023-03-08 20:38:05 +1100

    All Mario Animations

--- Assets/Scripts/MarioAnim.cs
+++ Assets/Scripts/MarioAnim.cs
@@ -10,13 +10,13 @@ public class MarioAnim : MonoBehaviour
     private Rigidbody2D Rigid2D;
     private PlayerManager MarioState;
     private PlayerMovement MarioMove;
-    private float FPS = 1 / 12;
+    public float FPS;
     public float Timer;
     [SerializeField] private PlayerData data;
     private bool IsRight = true;
     public bool NeedTurn = false;
     public bool Play = true;
-
+    
     private string[] Idle = new string[] { "Idle-Small", "Idle-Big", "Idle-Fire" };
     private string[] Walk = new string[] { "Walk-Small", "Walk-Big", "Walk-Fire" };
     private string[] Jump = new string[] { "Jump-Small", "Jump-Big", "Jump-Fire" };
@@ -25,6 +25,7 @@ public class MarioAnim : MonoBehaviour
     public int layer = 0;
     void Start()
     {
+        FPS = 0.1f;
         Timer = FPS;
         MarioAnimator = GetComponentInChildren<Animator>();
         Rigid2D = GetComponent<Rigidbody2D>();
@@ -35,6 +36,7 @@ public class MarioAnim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (MarioAnimator.GetCurrentAnimatorStateInfo(0).IsName("idle"))
         {
             Play = true;
@@ -42,10 +44,7 @@ public class MarioAnim : MonoBehaviour
         }
         if (!Play)
             return;
-        if (MarioState.PlayerState == 3 && Input.GetKeyDown(KeyCode.X))
-        {
-            MarioAnimator.Play("Fire-Fire");
-        }
+        
         if (MarioState.Invincible)
         {
             Timer -= Time.deltaTime;
@@ -75,16 +74,28 @@ public class MarioAnim : MonoBehaviour
                     MarioAnimator.SetLayerWeight(2, 0);
                     MarioAnimator.SetLayerWeight(3, 1);
                 }
-                Timer = FPS;
+                
+                Timer = MarioState.Timer < 2? FPS * 3    : FPS;
                 layer++;
                 layer %= 4;
             }
+        }
+        else
+        {
+            MarioAnimator.SetLayerWeight(1, 0);
+            MarioAnimator.SetLayerWeight(2, 0);
+            MarioAnimator.SetLayerWeight(3, 0);
         }
         if(Mathf.Abs(Rigid2D.velocity.x) > data.groundMaxSpeed * 0.4f)
         {
             NeedTurn = true;
         }
-        if (Rigid2D.velocity == Vector2.zero)
+        
+        if (MarioState.PlayerState == 3 && Input.GetKeyDown(KeyCode.X) || (MarioState.FireTimer >= 0 && MarioState.BurstCount == 2) || (MarioState.FireTimer >= MarioState.ShotDelay && MarioState.BurstCount == 1))
+        {
+            MarioAnimator.Play("Fire-Fire");
+        }
+        else if (Rigid2D.velocity == Vector2.zero)
         {
             MarioAnimator.Play(Idle[MarioState.PlayerState - 1]);
         }
@@ -95,27 +106,26 @@ public class MarioAnim : MonoBehaviour
         else if (MarioMove.IsJumping || Rigid2D.velocity.y != 0)
         {
             IsRight = MarioMove.IsFacingRight;
-            MarioAnimator.applyRootMotion = true;
             MarioAnimator.speed = 1;
             MarioAnimator.Play(Jump[MarioState.PlayerState - 1]);
         }
         else if (IsRight != MarioMove.IsFacingRight && NeedTurn == true && Mathf.Abs(Rigid2D.velocity.x) > data.groundMaxSpeed * 0.1f)
         {
-            MarioAnimator.applyRootMotion = false;
+            Debug.Log((IsRight != MarioMove.IsFacingRight) + " " + NeedTurn + (Mathf.Abs(Rigid2D.velocity.x) > data.groundMaxSpeed * 0.1f));
             MarioAnimator.speed = 1;
             MarioAnimator.Play(Turn[MarioState.PlayerState - 1]);
         }
         else
         {
-            if (Mathf.Abs(Rigid2D.velocity.x) < data.groundMaxSpeed * 0.1f)
-            {
-                NeedTurn = false;
-                Debug.Log("running2");
-            }
-            MarioAnimator.applyRootMotion = false;
+            
             IsRight = MarioMove.IsFacingRight;
             MarioAnimator.speed = 1;
             MarioAnimator.Play(Walk[MarioState.PlayerState - 1]);
+        }
+
+        if (Mathf.Abs(Rigid2D.velocity.x) < data.groundMaxSpeed * 0.1f)
+        {
+            NeedTurn = false;
         }
     }
 }
